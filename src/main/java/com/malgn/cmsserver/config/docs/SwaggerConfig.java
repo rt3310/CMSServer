d74529be4cc762c6a926cd4f7a61f27125b32c67
rt3310 <btac3310@gmail.com>
2026-03-12 14:11:33 +0900

    docs: README 수정

--- src/main/java/com/malgn/cmsserver/config/docs/SwaggerConfig.java
+++ src/main/java/com/malgn/cmsserver/config/docs/SwaggerConfig.java
@@ -5,15 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-
-    @Value("${client.url}")
-    private String clientUrl;
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -26,7 +22,6 @@ public class SwaggerConfig {
 
         return new OpenAPI()
                 .info(openApiInfo())
-//                .servers(List.of(new Server().url(clientUrl)))
                 .components(new Components().addSecuritySchemes(securitySchemeName, scheme))
                 .addSecurityItem(requirement);
     }
